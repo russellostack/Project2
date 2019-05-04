862a1b5462dc050eee26abc9610deff4282699a0
russellostack <32462495+russellostack@users.noreply.github.com>
2019-05-04 13:29:29 -0400

    sql works

--- db/schema.sql
+++ db/schema.sql
@@ -5,17 +5,17 @@ use exercise_db;
 create table users(
     id int not null auto_increment,
     username varchar(255) not null,
-    PASSWORD varchar(255) not null,
+    `password` varchar(255) not null,
     starting_weight int(10) not null,
     target_weight int(10) not null,
-    primary key (user_id)
+    primary key (id)
 );
 create table calories(
     id int not null auto_increment,
     food_name varchar(255) not null,
     food_quantity int(10) not null,
     total_cal_con int(10) not null,
-    foreign key (user_id) references users(id),
+    foreign key (id) references users(id),
     primary key (id)
 );
 create table activities(
@@ -23,11 +23,13 @@ create table activities(
     activity_name varchar(255) not null,
     activity_quantity int(10) not null,
     total_cal_burn int(10) not null,
-    foreign key (user_id) references users(id),
+    foreign key (id) references users(id),
     primary key (id)
 );
-create table weight(
+create table user_weight(
     id int not null auto_increment,
     user_weight int(10) not null,
-    input_time DATETIME
+    input_time DATETIME,
+    foreign key(id) REFERENCES(users),
+    primary key (id)
 );
