b1f922a29a0675fd6ba69526dc5275e56f8a8f6f
Fred Astaire <lancebc@thegodardgroup.com>
2019-05-09 17:56:46 -0400

    bits and pieces toward funcitonality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,44 @@
 var db = require("../models");
 
-module.exports = function(app) {
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    db.Users.findAll({}).then(function (data) {
+      var hbsObject = {
+        Users: data
+      };
+      console.log(data)
+      res.render("index", hbsObject);
+    });
+  });
+
+  app.get("/", function (req, res) {
+    db.activities.findAll({ where: { id: req.params.id } }).then(function (data) {
+      var hbsObject = {
+        Users: data
+      };
+      console.log(data)
+      res.render("index", hbsObject);
+    });
+  });
+
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/users", function (req, res) {
+    db.Users.findAll({}).then(function (dbUsers) {
+      res.json(dbUsers);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
