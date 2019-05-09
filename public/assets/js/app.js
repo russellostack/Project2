6ddb0a5379cc38c388db463435bf1ed0eb0f33a9
Igor Guba <gubaigor@gmail.com>
2019-05-09 19:01:33 -0400

    Add input.html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -26,9 +26,9 @@ $(document).ready(function () {
 
     // Click events ============================================================
     // Signing in:
-    $("#signinbtn").click(function () {
+    $("#sign-in-btn").click(function () {
         event.preventDefault();
-        $("#signInModal").modal("show");
+        $("#sign-in-modal").modal("show");
         $("#inputUserName").val("");
         $("#inputPassword").val("");
     });
@@ -37,126 +37,160 @@ $(document).ready(function () {
     $("#signinmodalbtn").click(function () {
         event.preventDefault();
         $("#rfq").empty();
-        $("#user-data").empty();
-        $("#signInModal").modal("hide");
+        $("#user-input-food").empty();
+        $("#sign-in-modal").modal("hide");
         userName = $("#inputUserName").val().trim();
-        var $newRow = $("<div>");
-        $newRow.attr("id", "user-data");
-        $newRow.addClass("row");
-        var $newDiv = $("<div>");
-        $newDiv.addClass("col-md-12");
-        var $newDiv2 = $("<div>");
-        $newDiv2.addClass("demo-content");
-        $newDiv.append($newDiv2);
-        var $newPar = $("<p>");
-        $newPar.attr("id", "hello");
-        $newPar.text("Hello, " + userName + "!");
-        $newDiv2.append($newPar);
-        // Adding input form for calories:
-        var newForm = $("<form>");
-        var divForm = $("<div>");
-        divForm.addClass("form-group");
-        var label1 = $("<label>Today's calories consumption</label>");
-        var input1 = $("<input>");
-        input1.addClass("form-control");
-        input1.attr("id", "input-cal");
-        input1.attr("placeholder", "input today's calories amount");
-        divForm.append(label1);
-        divForm.append(input1);
-        newForm.append(divForm);
-        $newDiv2.append(newForm);
+
+        // Food input ============================================================================================================
+        var foodRow = $("<div>");
+        foodRow.attr("id", "user-input-food");
+        foodRow.addClass("row");
+        var foodDiv = $("<div>");
+        foodDiv.addClass("col-md-6");
+        var newDiv = $("<div>");
+        newDiv.addClass("main-content");
+        var newPar = $("<p>");
+        newPar.attr("id", "hello");
+        newPar.text("Hello, " + userName + "!");
+        newDiv.append(newPar);
+        foodDiv.append(newDiv);
+        foodRow.append(foodDiv);
+        $(".container").append(foodRow);
+
+        // Input form for food type:
+        var foodForm = $("<form>");
+        var foodInputDiv = $("<div>");
+        foodInputDiv.addClass("form-group");
+
+        var labelFoodType = $("<label>Enter types of food you ate today</label>");
+        var inputFoodType = $("<input>");
+        inputFoodType.addClass("form-control");
+        inputFoodType.attr("id", "input-food-type");
+        inputFoodType.attr("placeholder", "food type, etc. chicken");
+
+        var labelFoodAmt = $("<label>Enter amount of food you ate today</label>");
+        labelFoodAmt.addClass("mt-3");
+        var inputFoodAmt = $("<input>");
+        inputFoodAmt.addClass("form-control");
+        inputFoodAmt.attr("id", "input-food-amount");
+        inputFoodAmt.attr("placeholder", "food amount, g");
+
+        foodInputDiv.append(labelFoodType);
+        foodInputDiv.append(inputFoodType);
+        foodInputDiv.append(labelFoodAmt);
+        foodInputDiv.append(inputFoodAmt);
+        foodForm.append(foodInputDiv);
+        newDiv.append(foodForm);
+
+        // newForm.append(divForm);
+        // $newDiv2.append(newForm);
+
+        // Input form for food amount:
+        // var newForm_ = $("<form>");
+        // var divForm_ = $("<div>");
+        // divForm_.addClass("form-group");
+        // var label0 = $("<label>Today's calories consumption</label>");
+        // var input0 = $("<input>");
+        // input0.addClass("form-control");
+        // input0.attr("id", "input-cal");
+        // input0.attr("placeholder", "input today's calories amount");
+        // divForm_.append(label0);
+        // divForm_.append(input0);
+        // newForm_.append(divForm_);
+        // $newDiv2.append(newForm_);
+        //==========================================================================================================
+
         // Adding input form for exercise type:
-        var newForm2 = $("<form>");
-        var divForm2 = $("<div>");
-        divForm2.addClass("form-group");
-        var label2 = $("<label>Exercise</label>");
-        var input2 = $("<input>");
-        input2.addClass("form-control");
-        input2.attr("id", "input-ex");
-        input2.attr("placeholder", "input type of exercise you're doing today");
-        divForm2.append(label2);
-        divForm2.append(input2);
-        newForm2.append(divForm2);
-        $newDiv2.append(newForm2);
+        // var newForm2 = $("<form>");
+        // var divForm2 = $("<div>");
+        // divForm2.addClass("form-group");
+        // var label2 = $("<label>Exercise</label>");
+        // var input2 = $("<input>");
+        // input2.addClass("form-control");
+        // input2.attr("id", "input-ex");
+        // input2.attr("placeholder", "input type of exercise you're doing today");
+        // divForm2.append(label2);
+        // divForm2.append(input2);
+        // newForm2.append(divForm2);
+        // $newDiv2.append(newForm2);
+
         // Adding input form for exercise amount:
-        var newForm3 = $("<form>");
-        var divForm3 = $("<div>");
-        divForm3.addClass("form-group");
-        var label3 = $("<label>Exercise amount</label>");
-        var input3 = $("<input>");
-        input3.addClass("form-control");
-        input3.attr("id", "input-ex-am");
-        input3.attr("placeholder", "input anount of exercise you're doing today");
-        divForm3.append(label3);
-        divForm3.append(input3);
-        newForm3.append(divForm3);
-        $newDiv2.append(newForm3);
+        // var newForm3 = $("<form>");
+        // var divForm3 = $("<div>");
+        // divForm3.addClass("form-group");
+        // var label3 = $("<label>Exercise amount</label>");
+        // var input3 = $("<input>");
+        // input3.addClass("form-control");
+        // input3.attr("id", "input-ex-am");
+        // input3.attr("placeholder", "input anount of exercise you're doing today");
+        // divForm3.append(label3);
+        // divForm3.append(input3);
+        // newForm3.append(divForm3);
+        // $newDiv2.append(newForm3);
         // Adding input form for current weight:
-        var newForm4 = $("<form>");
-        var divForm4 = $("<div>");
-        divForm4.addClass("form-group");
-        var label4 = $("<label>Current weight</label>");
-        var input4 = $("<input>");
-        input4.addClass("form-control");
-        input4.attr("id", "input-cur-weight");
-        input4.attr("placeholder", "input your current weight");
-        divForm4.append(label4);
-        divForm4.append(input4);
-        newForm4.append(divForm4);
-        $newDiv2.append(newForm4);
+        // var newForm4 = $("<form>");
+        // var divForm4 = $("<div>");
+        // divForm4.addClass("form-group");
+        // var label4 = $("<label>Current weight</label>");
+        // var input4 = $("<input>");
+        // input4.addClass("form-control");
+        // input4.attr("id", "input-cur-weight");
+        // input4.attr("placeholder", "input your current weight");
+        // divForm4.append(label4);
+        // divForm4.append(input4);
+        // newForm4.append(divForm4);
+        // $newDiv2.append(newForm4);
+
         // Adding submit button for daily input form:
-        var newBtn2 = $("<button>");
-        newBtn2.attr("id", "daily-submit");
-        newBtn2.attr("type", "button");
-        newBtn2.addClass("btn btn-primary btn-lg");
-        newBtn2.text("Submit");
-        $newDiv2.append(newBtn2);
+        // var newBtn2 = $("<button>");
+        // newBtn2.attr("id", "daily-submit");
+        // newBtn2.attr("type", "button");
+        // newBtn2.addClass("btn btn-primary btn-lg");
+        // newBtn2.text("Submit");
+        // $newDiv2.append(newBtn2);
 
         // newForm.append("<div class='form-group'>");
 
-        $newRow.append($newDiv);
-        $(".container").append($newRow);
+        // $newRow.append($newDiv);
+        // $(".container").append($newRow);
 
         // if Sign in success(!) - create "Sing out" button:
-        $("#signinbtn").detach();
-        $("#newuserbnt").detach();
-        var newBtn = $("<button>");
-        newBtn.attr("id", "signoutbtn");
-        newBtn.attr("type", "button");
-        newBtn.addClass("btn btn-primary btn-lg");
-        newBtn.text("Sign out");
-        $("#buttons").append(newBtn);
+        // $("#signinbtn").detach();
+        // $("#newuserbnt").detach();
+        // var newBtn = $("<button>");
+        // newBtn.attr("id", "signoutbtn");
+        // newBtn.attr("type", "button");
+        // newBtn.addClass("btn btn-primary btn-lg");
+        // newBtn.text("Sign out");
+        // $("#buttons").append(newBtn);
 
         // Click event for "Signing Out" button:
-        $("#signoutbtn").click(function () {
-            event.preventDefault();
-            // console.log("here");
-            location.reload(true);
-        });
+        // $("#signoutbtn").click(function () {
+        //     event.preventDefault();
+        //     // console.log("here");
+        //     location.reload(true);
+        // });
 
         // Click event for "Submit" (daily data input) button:
-        $("#daily-submit").click(function () {
-            // event.preventDefault();
-            console.log("daily-submit");
-            $("#user-data").empty();
-            // $("#signInModal").modal("hide");
-            // var userName = $("#inputUserName").val().trim();
-            var $newRow = $("<div>");
-            $newRow.attr("id", "user-data");
-            $newRow.addClass("row");
-            var $newDiv = $("<div>");
-            $newDiv.addClass("col-md-12");
-            var $newDiv2 = $("<div>");
-            $newDiv2.addClass("demo-content");
-            $newDiv.append($newDiv2);
-            var $newPar = $("<p>");
-            $newPar.attr("id", "progress");
-            $newPar.text(userName + ", your progress is here:");
-            $newDiv2.append($newPar);
-            $newDiv2.append('<img id="fitchart" class="img-fluid" src="../public/assets/images/fitness-chart.jpg"/>');
-            $newRow.append($newDiv);
-            $(".container").append($newRow);
-        });
+        // $("#daily-submit").click(function () {
+        //     console.log("daily-submit");
+        //     $("#user-data").empty();
+        //     var $newRow = $("<div>");
+        //     $newRow.attr("id", "user-data");
+        //     $newRow.addClass("row");
+        //     var $newDiv = $("<div>");
+        //     $newDiv.addClass("col-md-12");
+        //     var $newDiv2 = $("<div>");
+        //     $newDiv2.addClass("main-content");
+        //     $newDiv.append($newDiv2);
+        //     var $newPar = $("<p>");
+        //     $newPar.attr("id", "progress");
+        //     $newPar.text(userName + ", your progress is here:");
+        //     $newDiv2.append($newPar);
+        //     $newDiv2.append('<img id="fitchart" class="img-fluid" src="../public/assets/images/fitness-chart.jpg"/>');
+        //     $newRow.append($newDiv);
+        //     $(".container").append($newRow);
+        // });
     });
 
     // Creating new user
