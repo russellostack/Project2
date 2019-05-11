3c757d2c9759b1400a1639ddce6e45ea2b5d4fc1
russellostack <32462495+russellostack@users.noreply.github.com>
2019-05-11 09:37:35 -0400

    api routes updated, html routes updated and need to be connected to buttons, need guidance on api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,9 @@ var db = require("../models");
 
 
 module.exports = function (app) {
-  app.get("/", function (req, res) {
+
+
+  app.get("/api/users/", function (req, res) {
     db.Users.findAll({}).then(function (data) {
       var hbsObject = {
         Users: data
@@ -12,34 +14,70 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/", function (req, res) {
-    db.activities.findAll({ where: { id: req.params.id } }).then(function (data) {
+
+  app.get("/api/users/;", function (req, res) {
+    db.activities.findAll({
+       where: 
+      { id: req.params.id }
+     }).then(function (data) {
       var hbsObject = {
         Users: data
       };
-      console.log(data)
       res.render("index", hbsObject);
     });
   });
 
-  // Get all examples
-  app.get("/api/users", function (req, res) {
-    db.Users.findAll({}).then(function (dbUsers) {
-      res.json(dbUsers);
+  //new user api post
+  app.post("/api/users", function(req,res){
+    db.Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      username: req.body.username,
+      password: req.body.password,
+      starting_weight: req.body.starting_weight,
+      target_weight:req.body.target_weight
+      //  
+      //not sure how to initialize the created_at and updated_at fields
+
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
-      res.json(dbExample);
+  // user based calorie input post
+  app.post("/api/calories/:user_id", function(req,res){
+    db.Post.create({
+      title:req.body.title,
+      body: req.body.body,
+      user_id: req.body.user_id,
+      food_name: req.body.food_name,
+      food_date: req.body.food_date,
+      total_cal_con: req.body.total_cal_con
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-      res.json(dbExample);
+  //user based activity input post
+  app.post("/api/activities/:user_id", function(req,res){
+    db.Post.create({
+      title:req.body.title,
+      body: req.body.body,
+      user_id: req.body.user_id,
+      activity_name: req.body.activity_name,
+      activity_quantity: req.bdy.activity_quantity,
+      actifity_date: req.body.activity_date,
+      total_cal_burn: req.body.total_cal_burn
     });
   });
+
+  //"update" current user weight by posting to user weight table.
+  app.post("/api/user_weight/:user_id", function(req,res){
+    db.Post.create({
+      title:req.body.title,
+      body: req.body.body,
+      user_id: req.body.user_id,
+      user_weight: req.body.user_weight,
+      input_time: req.body.input_time
+    })
+  })
+
+
+  
 };
