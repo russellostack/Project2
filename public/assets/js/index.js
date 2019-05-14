bc7b10500fc26c364f90bc04070820f492e9f081
russellostack <32462495+russellostack@users.noreply.github.com>
2019-05-14 18:28:34 -0400

    changes to api routes, cleaned app.js and adding api function collection to it, connecting it to api routes, button on clicks next

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,5 @@
+import { json } from "sequelize/types";
+
 $(document).ready(function () {
 
     // Random quotation module ================================================
@@ -65,6 +67,45 @@ $(document).ready(function () {
 
     yearGenerator();
 
+
+
+    var api = {
+        signIn: function(user){
+            return $.ajax({
+                headers: {"Content-type":"application/json"},
+                url: "api/signin",
+                type: "POST",
+                data: user
+            })
+        },
+        caloriePost: function(data, user_id){
+            return $.ajax({
+                headers: {"Content-type":"application/json"},
+                url: "api/calorie/"+user_id,
+                type: "POST",
+                data: data
+            })
+        },
+        activityPost: function(data, user_id){
+            return $.ajax({
+                headers: {"Content-type":"application/json"},
+                url: "api/activity"+user_id,
+                data: data
+            })
+        },
+        userweightPost: function(data, user_id){
+            return $.ajax({
+                headers: {"Content-type":"application/json"},
+                url: "api/userweight/"+user_id,
+                data: data
+            })
+        },
+
+
+    };
+
+
+
     // Click events ============================================================
     // Signing in button:
     $("#sign-in-btn").click(function () {
@@ -109,4 +150,25 @@ $(document).ready(function () {
             });
         });
     });
+
+    /// food api call button ////
+
+    $("#food-button").click(function () {
+        event.preventDefault();
+        var foodName = {
+            name: $("#food-input").val().trim(),
+        };
+        foodName.replace(" ", "%20");
+        var queryURL = "'https://api.edamam.com/api/food-database/parser?ingr=" + foodName + "&app_id={2cea8c5e}&app_key={3742da7bb611e71fab3e49e361fdbb55}"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            total_cal_con = response.hints[0].food.nutrients.ENERC_KCAL;
+            food_name = response.hints[0].food.label;
+        });
+    });
+
+
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,11 +5,8 @@ module.exports = function (app) {
 
   //gets list of users and displays it on the sign in page -  not needed if we use passport
   app.get("/api/home/", function (req, res) {
-    db.Users.findAll({}).then(function (data) {
-      var hbsObject = {
-        Users: data
-      };
-      res.json(hbsObject);
+    db.Users.findAll({}).then(function (dbUsers) {
+      res.json(dbUsers);
     });
   });
 
@@ -23,15 +20,8 @@ module.exports = function (app) {
       order: [['created_At', 'DESC']],
       where:
         { user_id: req.params.user_id }
-    }).then(function (data) {
-      var activities_hbsObject = {
-        Activities: data
-      };
-      var totalCalories = 0;
-      for (var i = 0; i < data.length; i++) {
-        totalCalories = (totalCalories + parseInt(data[i].total_cal_burn));
-      }
-      res.json(activities_hbsObject);
+    }).then(function (dbActivities) {
+      res.json(dbActivities);
     });
   });
 
@@ -44,11 +34,8 @@ module.exports = function (app) {
       order: [['created_At', 'DESC']],
       where:
         { user_id: req.params.user_id }
-    }).then(function (data) {
-      var calories_hbsObject = {
-        Calories: data
-      };
-      res.json(calories_hbsObject);
+    }).then(function (dbCalories) {
+      res.json(dbCalories);
     });
   });
 
@@ -60,11 +47,8 @@ module.exports = function (app) {
       order: [['createdAt', 'DESC']],
       where:
         { user_id: req.params.user_id }
-    }).then(function (data) {
-      var user_weight_hbsObject = {
-        user_weight: data
-      };
-      res.json(user_weight_hbsObject);
+    }).then(function (dbUserweight) {
+      res.json(dbUserweight);
     });
   });
 
@@ -73,7 +57,7 @@ module.exports = function (app) {
 ////////// POSTS ///////////
 
   //new user api post
-  app.post("/api/input", function (req, res) {
+  app.post("/api/signin", function (req, res) {
     db.users.create({
       username: req.body.username,
       password: req.body.password,
@@ -82,38 +66,45 @@ module.exports = function (app) {
       //  
       //not sure how to initialize the created_at and updated_at fields
 
+    }).then(function(dbUser){
+      res.json(dbUser);
     });
   });
 
   // user specific calorie input post
-  app.post("/api/input/:user_id", function (req, res) {
+  app.post("/api/calorie/:user_id", function (req, res) {
     db.calories.create({
       user_id: req.body.user_id,
       food_name: req.body.food_name,
       food_date: req.body.food_date,
       total_cal_con: req.body.total_cal_con
-    });
+    }).then(function(dbCalories){
+      res.json(dbCalories);
+    })
   });
 
   //user based activity input post
-  app.post("/api/input/:user_id", function (req, res) {
+  app.post("/api/activity/:user_id", function (req, res) {
     db.activities.create({
       user_id: req.body.user_id,
       activity_name: req.body.activity_name,
       activity_quantity: req.bdy.activity_quantity,
       actifity_date: req.body.activity_date,
       total_cal_burn: req.body.total_cal_burn
+    }).then(function(dbActivities){
+      res.json(dbActivities);
     });
   });
 
   //"update" current user weight by posting to user weight table.
-  app.post("/api/input/:user_id", function (req, res) {
+  app.post("/api/userweight/:user_id", function (req, res) {
     db.user_weight.create({
       user_id: req.body.user_id,
       user_weight: req.body.user_weight,
       input_time: req.body.input_time,
-
-    })
+    }).then(function(dbUserweight){
+      res.json(dbUserweight);
+    });
   });
 
 /////////// should move to app.js /////////
