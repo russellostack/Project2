339a74d65c3baddaf4909848b56e19aa7d7fc8da
russellostack <32462495+russellostack@users.noreply.github.com>
2019-05-16 18:45:17 -0400

    buttons might work

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -146,14 +146,14 @@ $(document).ready(function () {
         /////////////// page change button listeners /////////
 
         $('.inputpagelink').click(function () {
-            window.location.href('https://fit-or-fatapp.herokuapp.com/input');
+            window.location = 'https://fit-or-fatapp.herokuapp.com/input';
         });
         $(".chartspagelink").click(function () {
             // api.userdataGet(user_id);
-            window.location.href("https://fit-or-fatapp.herokuapp.com/charts")
+            window.location = "https://fit-or-fatapp.herokuapp.com/charts";
         });
         $(".homepagelink").click(function () {
-            window.location.href("https://fit-or-fatapp.herokuapp.com/")
+            window.location = "https://fit-or-fatapp.herokuapp.com/";
         });
 
         //////// new user modal button listener /////////////
